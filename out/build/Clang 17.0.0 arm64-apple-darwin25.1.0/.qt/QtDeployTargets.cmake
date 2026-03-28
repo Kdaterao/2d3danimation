c35fdace6bb5654c331baa3fa6674f4d68f27a72
kdaterao <akdaterao@gmail.com>
2026-03-27 20:02:56 -0400

    did something?

--- out/build/Clang 17.0.0 arm64-apple-darwin25.1.0/.qt/QtDeployTargets.cmake
+++
@@ -1,4 +0,0 @@
-set(__QT_DEPLOY_TARGET_2d3danimation_FILE /Users/krish/codingStuff/2d3danimation/out/build/Clang 17.0.0 arm64-apple-darwin25.1.0/2d3danimation.app/Contents/MacOS/2d3danimation)
-set(__QT_DEPLOY_TARGET_2d3danimation_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_2d3danimation_tests_FILE /Users/krish/codingStuff/2d3danimation/out/build/Clang 17.0.0 arm64-apple-darwin25.1.0/2d3danimation_tests)
-set(__QT_DEPLOY_TARGET_2d3danimation_tests_TYPE EXECUTABLE)
--- out/build/Clang 17.0.0 arm64-apple-darwin25.1.0/CMakeFiles/2d3danimation_autogen_timestamp_deps.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for 2d3danimation_autogen_timestamp_deps.
-# This may be replaced when dependencies are built.
